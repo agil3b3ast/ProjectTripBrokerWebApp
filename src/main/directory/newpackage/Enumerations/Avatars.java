41e756417c0ce9938750c0210904df02a3fa11da
agil3b3ast <agil3b3ast@hotmail.it>
2016-02-08 17:07:53 +0100

    Added new style

--- src/main/directory/newpackage/Enumerations/Avatars.java
+++ src/main/directory/newpackage/Enumerations/Avatars.java
@@ -8,10 +8,10 @@ import newpackage.EntityPackage.OffertaEvento;
 public enum Avatars{
 
     WebClient("C:\\Users\\Alessandro\\Downloads\\Annotations, Hibernate e JPA\\MyFirstWebApp\\src\\main\\webapp\\biffyclyro.jpeg"),
-    Packet("C:\\Users\\Alessandro\\Documents\\ProgettoTripBroker\\ProjectTripBrokerWebApp\\web\\random_points.jpg"),
-    Evento("C:\\Users\\Alessandro\\Documents\\ProgettoTripBroker\\ProjectTripBrokerWebApp\\web\\evento.jpg"),
-    Pernotto("C:\\Users\\Alessandro\\Documents\\ProgettoTripBroker\\ProjectTripBrokerWebApp\\web\\pernotto.jpg"),
-    Trasporto("C:\\Users\\Alessandro\\Documents\\ProgettoTripBroker\\ProjectTripBrokerWebApp\\web\\trasporto.jpg");
+    Packet("random_points.jpg"),
+    Evento("evento.jpg"),
+    Pernotto("pernotto.jpg"),
+    Trasporto("trasporto.jpg");
     private String path;
 
     Avatars(String path){
